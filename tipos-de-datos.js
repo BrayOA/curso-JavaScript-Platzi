74896ff8d9744c89a480cf8d5170c5b8181e5746
BrayOA <borconarellano@gmail.com>
2024-08-07 17:00:47 -0500

    javaScript-bases

---
+++ tipos-de-datos.js
@@ -0,0 +1,28 @@
+// Primitivos
+
+//String
+let nombre = "tere"
+//Number
+let edad = 24
+//Boolean
+let esMayorDeEdad = true
+//null
+let noHayValor = null 
+//undefined
+let noDefinido = undefined
+//symbolo
+let simboloUnico = Symbol("unico")
+//bigint
+let numeroGrande = 2n
+
+//COMPLEJOS
+
+//object
+let carro = {
+    marca: "tesla",
+    model: "model S"
+}
+//array
+let frutas = ["manzana","platano","uvas"]
+//function
+function saludar () {}
---
+++ variables.js
@@ -0,0 +1,14 @@
+let cajaDeAndy= "Woody"
+console.log(cajaDeAndy);
+
+// LO NO PERMITIDO//
+
+let c = "woody"
+let cda = "woody"
+let pcAndy = "woody"
+
+//LO PERMITIDO//
+
+let primerTrasteoDeAndy = "woody"
+let urlDelUsuario = "https://www.google.com"
+let idDelUsuario = "123456"
